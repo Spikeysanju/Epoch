eb141d3db323234d048e789c048573d037640ff1
Sanju S <spikeysanju98@gmail.com>
2021-03-25 17:11:01 +0530

    Add PinView
    Signed-off-by: Sanju S <spikeysanju98@gmail.com>

--- app/src/main/java/com/example/androiddevchallenge/components/TopBar.kt
+++ app/src/main/java/com/example/androiddevchallenge/components/TopBar.kt
@@ -15,10 +15,13 @@
  */
 package com.example.androiddevchallenge.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -33,4 +36,15 @@ fun TopBar() {
         style = MaterialTheme.typography.h4,
         color = pinkText
     )
+}
+
+@Composable
+fun Toolbar() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        TopBar()
+    }
 }
