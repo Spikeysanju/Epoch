030b7ce5f0801318cacde7d2d10f6b9ab9915c7b
Sanju S <spikeysanju98@gmail.com>
2021-03-25 17:19:16 +0530

    Add PinView
    Signed-off-by: Sanju S <spikeysanju98@gmail.com>

--- app/src/main/java/com/example/androiddevchallenge/components/Timer.kt
+++ app/src/main/java/com/example/androiddevchallenge/components/Timer.kt
@@ -54,7 +54,6 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.inset
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.R
@@ -249,10 +248,4 @@ fun CabelView(viewModel: MainViewModel, isRunning: Boolean) {
             }
         }
     }
-}
-
-@Preview
-@Composable
-fun preview() {
-    Pin()
 }
