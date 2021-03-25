5976ba93077f596ef5c254bcb61d409eeb8965be
Sanju S <spikeysanju98@gmail.com>
2021-03-25 22:42:17 +0530

    Create android_ci.yml & lint.yml #15
    Signed-off-by: Sanju S <spikeysanju98@gmail.com>

--- app/src/main/java/dev/spikeysanju/epoch/utils/getFormattedTime.kt
+++ app/src/main/java/dev/spikeysanju/epoch/utils/getFormattedTime.kt
@@ -21,7 +21,7 @@ import java.util.concurrent.TimeUnit
 fun getFormattedTime(millis: Long): String {
     val minutes = TimeUnit.MILLISECONDS.toMinutes(millis)
     val seconds = TimeUnit.MILLISECONDS.toSeconds(millis) -
-            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis))
+        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis))
 
     val minutesFormatted = if (minutes.toString().length == 1) "0$minutes" else minutes.toString()
     val secondsFormatted = if (seconds.toString().length == 1) "0$seconds" else seconds.toString()
